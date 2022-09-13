8ab48e1399b3a26aadabe9ac49a9ae32d3e7689f
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-13 01:59:39 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - print alphabets in lower case
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets++);
+		putchar("\n");
+	return (0);
+}
