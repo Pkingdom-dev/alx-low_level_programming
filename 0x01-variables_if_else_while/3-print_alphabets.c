8954cf302ecf8da253b86ea42c5956f7c674d588
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-13 03:52:20 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,13 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'Z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(letters);
 	}
 	putchar('\n');
 	return(0);
