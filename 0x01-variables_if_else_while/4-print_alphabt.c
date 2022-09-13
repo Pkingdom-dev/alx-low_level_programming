3811164a2028c3e5f30ded1809ae35d3bc21afca
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-13 04:24:46 -0700

    done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,16 +5,16 @@
  * Description - print all letters execept 'q' and 'e'
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	int letters;
 	for (letters = 'a'; letters <= 'z'; letters++)
 	{
-		if (letters != 'q' && letters != 'e')
+		if (letters != 'e' && letters != 'q')
 		{
 			putchar(letters);
 		}
 	}
-	putchar(letters);
+	putchar('\n');
 	return (0);
 }
