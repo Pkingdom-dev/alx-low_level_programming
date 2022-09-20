64842a1d7b28e0a143ea7eb5aabb6c713f78abee
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-19 20:30:20 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Nested Loops
+ * Description - print all possible of different combination of two digit.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int first_digit second_digit;
+
+	for (first_digit = '0'; first_digit <= '9'; first_digit++)
+	{
+		for (second_digit = '0'; second_digit <= '9'; second_digit++)
+		{
+			if (first_digit != second_digit)
+			{
+				putchar(second_digit);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	
+	}
+	putchar('\n');
+	return (0);
+}
