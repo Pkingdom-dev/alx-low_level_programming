ef9da0b3dc7f0a82160df0bc471a63d339e7c8d9
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-20 08:40:01 -0700

    done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,24 +8,17 @@ int main(void)
 {
 	int first_digit, second_digit, third_digit;
 
-	for (first_digit = '0'; first_digit <= '9'; first_digit++)
+	for (first_digit = '0'; first_digit <= '7'; first_digit++)
 	{
-		for (second_digit = '0'; second_digit <= '9'; second_digit++)
+		for (second_digit = '1'; second_digit <= '8'; second_digit++)
 		{
-			for (third_digit = '0'; third_digit <= '9'; third_digit++)
+			for (third_digit = '2'; third_digit <= '9'; third_digit++)
 			{
-				if ((first_digit < second_digit) && (first_digit < third_digit) && (second_digit < third_digit))
-				{
 					putchar(first_digit);
 					putchar(second_digit);
 					putchar(third_digit);
-					
-					if (third_digit != '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+					putchar(',');
+					putchar(' ');
 			}
 		}
 	}
