d32299dd60f9e5979ed615227059e11ff7aea1cd
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-19 20:08:15 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Description - print all possible of a single digits.
+ * Return: Aways 0 (success)
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = '0'; num <= '9'; num+)
+	{
+		putchar(num);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n')
+	return (0);}
