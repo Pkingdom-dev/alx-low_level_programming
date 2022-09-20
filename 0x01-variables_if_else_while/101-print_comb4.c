0ae85f154dd5040368a83e6c23c6b85536342e73
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-19 21:03:52 -0700

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - More Nested Loops
+ * Description -  prints all possible different combinations of three digits.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int first_digit, second_digit, third_digti;
+
+	for (first_digit = '0'; first_digit <= '9'; first_digit++)
+	{
+		for (second_digit = '0'; second_digit <= '9'; second_digit++)
+		{
+			for (third_digit = '0'; third_digit <= '9'; third_digit++)
+			{
+				if ((first_digit < second_digit) && (first_digit < thrid_digit) && (second_digit < third_digit))
+				{
+					putchar(first_digit);
+					putchar(second_digit);
+					putchar(third_digit);
+					
+					if (third_digit != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+
+	return (0);
+}
