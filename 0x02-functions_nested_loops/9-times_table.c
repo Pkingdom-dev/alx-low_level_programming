a31a2e1894207bb98d0006e00b0087b081d7b086
Samuel-Adetogun <jennyadetogun@gmail.com>
2022-09-22 01:50:51 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,18 +5,16 @@
  */
 void times_table(void)
 {
-	int i, j, multiply;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			multiply = i * j;
-
-			_putchar(multiply);
+			_putchar(i * j);
 			_putchar(',');
 			_putchar(' ');
-			_putchar('\n');
 		}
+		_putchar('\n')
 	}
 }
